8731268d5bb2df07438602ac6cbaf4db787c1283
Carp Robert Valentin <robert.carp@crystal-system.eu>
2025-01-13 17:54:06 +0200

    End of section 7

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,13 +3,12 @@ import { RouterOutlet } from '@angular/router';
 
 import { NavComponent } from "./nav/nav.component";
 import { AccountService } from './_services/account.service';
-import { HomeComponent } from "./home/home.component";
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, NavComponent, HomeComponent]
+    imports: [RouterOutlet, NavComponent]
 })
 export class AppComponent implements OnInit {
   private accountService = inject(AccountService);
