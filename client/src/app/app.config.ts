af93ddeebee453420b90f2ea6bbfd76ed79aa1f0
Carp Robert Valentin <robert.carp@crystal-system.eu>
2025-01-13 13:55:56 +0200

    End of section 6

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -4,11 +4,15 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
+import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
-    provideAnimations()
+    provideAnimations(),
+    provideToastr({
+      positionClass: 'toast-bottom-right'
+    })
   ]
 };
--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -27,7 +27,7 @@ export class HomeComponent implements OnInit {
   }
 
   getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe({
+    this.http.get('https://localhost:5081/api/users').subscribe({
       next: response => this.users = response,
       error: error => console.log(error),
       complete: () => console.log('Request has completed')
